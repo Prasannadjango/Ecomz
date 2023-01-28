3934439de747136bc49d2c580f7b2a8717233639
Prasannadjango <Prasannakumarprasanna1199@gmail.com>
2023-01-28 23:39:43 +0530

    first commit

---
+++ Scripts/main.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 7,
+    spaceBetween: 30,
+    loop: true,
+});
+
+var swiper = new Swiper(".FlashdealSwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+      },
+    slidesPerView: 2,
+    spaceBetween: 30,
+    loop: true,
+    
+});
+
+
+var swiper = new Swiper(".TopDealsSwiper", {
+    
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    
+});
