eb0bbe944c64ec4a134b40322d99f18a75d59858
Prasannadjango <Prasannakumarprasanna1199@gmail.com>
2023-01-29 10:23:55 +0530

    changes

--- Scripts/main.js
+++ Scripts/main.js
@@ -16,12 +16,13 @@ var swiper = new Swiper(".FlashdealSwiper", {
 
 
 var swiper = new Swiper(".HotDealsSwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-      },
+   
     slidesPerView: 2,
     spaceBetween: 30,
     loop: true,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //   },
     
 });
 
@@ -32,3 +33,26 @@ var swiper = new Swiper(".TopDealsSwiper", {
     loop: true,
     
 });
+
+
+
+
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
